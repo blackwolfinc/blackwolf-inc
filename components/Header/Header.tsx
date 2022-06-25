b3722a87a49c8c4e2ad755cd71d5b7a2e488fb97
blackwolf inc <inc.blackwolf@gmail.com>
2022-06-25 23:59:49 +0700

    add new feature

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,23 +9,16 @@ export const Header = () => {
 
   return (
     <>
-    {/* Mobile Header  */}
+      {/* Mobile Header  */}
       <MediaQuery maxWidth={768}>
-      <div className="p-6 bg-transparent flex z-50 w-full space-x-7 fixed text-stone-200  bg-gradient-to-br from-[#323232] via-[#4b4b4bfb] to-[#0807079f] tracking-widest">
-          <Link href={"/"}>Homepage</Link>
-          {/* <Link href={"/owner"}>Owner</Link> */}
-          <Link href={"/colelction"}>Colection</Link>
-          {/* <Link href={"/project"}>Projects</Link> */}
-          <Link href={"/aboutus"}>About Us</Link>
-        </div>
-      </MediaQuery>
-      {/* Dexstop Header */}
-      <MediaQuery minWidth={768}>
+        <div className="p-4 bg-transparent flex z-50 w-full fixed  flex  justify-between items-center text-stone-200  bg-gradient-to-br from-[#323232] via-[#4b4b4b] to-[#0807079f] tracking-widest">
+          <h1 className="text-2xl">BWT.ID</h1>
+
           <div
             onClick={() => {
               setHeader(!Header);
             }}
-            className="absolute left-[7rem] top-[3rem] p-5 border-2 border-[#8c8c8c] rounded-[50%] z-50 text-white shadow-md shadow-[#8c8c8c] "
+            className="  top-[3rem] p-2 border-2 border-[#8c8c8c] rounded-[50%] z-50 text-white shadow-md shadow-[#8c8c8c]   bg-gradient-to-br from-[#3232322c] via-[#4b4b4bfb] to-[#0807073d]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -41,29 +34,74 @@ export const Header = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
+          </div>
+        </div>
+        {Header ? (
+          <div className=" border-[#cacaca] text-stone-200  bg-gradient-to-br from-[#323232] via-[#4b4b4bfb] to-[#0807079f]">
+            <div className="flex flex-col fixed mt-20 justify-center text-center  bg-gradient-to-br from-[#323232] via-[#4b4b4bfb] to-[#080707] w-full h-full z-50">
+              <div className="my-3  hover:scale-125 hover:ml-4 duration-100	min-w-max">
+                {" "}
+                <Link href={"/"}>Homepage</Link>
+              </div>
+              <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
+                {" "}
+                <Link href={"/colelction"}>Colection</Link>
+              </div>
+              <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
+                {" "}
+                <Link href={"/aboutus"}>About Us</Link>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </MediaQuery>
+      {/* Dexstop Header */}
+      <MediaQuery minWidth={768}>
+        <div
+          onClick={() => {
+            setHeader(!Header);
+          }}
+          className="absolute left-[7rem] top-[3rem] p-5 border-2 border-[#8c8c8c] rounded-[50%] z-50 text-white shadow-md shadow-[#8c8c8c] "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
 
-            {Header ? (
-              <div className="absolute mt-7 ml-[-0.5rem] flex-row h-max   border-l-[1px] pl-3 border-dashed border-[#cacaca]">
-                <div className="flex flex-col ">
-                  <div className="my-3 hover:scale-125 hover:ml-4 duration-100	min-w-max">
-                    {" "}
-                    <Link href={"/"}>Homepage</Link>
-                  </div>
-                  <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
-                    {" "}
-                    <Link href={"/colelction"}>Colection</Link>
-                  </div>
-                  <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
-                    {" "}
-                    <Link href={"/aboutus"}>About Us</Link>
-                  </div>
+          {Header ? (
+            <div className="absolute mt-7 ml-[-0.5rem] flex-row h-max   border-l-[1px] pl-3 border-dashed border-[#cacaca]">
+              <div className="flex flex-col ">
+                <div className="my-3 hover:scale-125 hover:ml-4 duration-100	min-w-max">
+                  {" "}
+                  <Link href={"/"}>Homepage</Link>
+                </div>
+                <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
+                  {" "}
+                  <Link href={"/colelction"}>Colection</Link>
+                </div>
+                <div className="my-3 hover:scale-125 hover:ml-4 duration-100 min-w-max">
+                  {" "}
+                  <Link href={"/aboutus"}>About Us</Link>
                 </div>
               </div>
-            ) : (
-              <></>
-            )}
-          </div>
-        </MediaQuery>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </MediaQuery>
     </>
   );
 };
--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -4,49 +4,48 @@ import React from "react";
 import { Footer2 } from "../components/Footer2/Footer";
 import { Layout } from "../components/layout/Layout";
 import image from "../public/assets/img/wolf3.png";
+import bagas from "../public/assets/img/bagas.png";
 
 const aboutus: NextPage = () => {
+  const reanderCard = () => {
+    const a = [1, 2, 3];
 
+    return a.map((value) => {
+      return (
+        <>
+          <div className="flex p-5 border-[1px] border-[#676767]  flex-col  rounded-md  flex justify-center  content-center self-center text-center shadow-2xl  text-stone-200">
+            <h1 className="text-2xl">This Title </h1>
+            <Image src={image} />
 
-  const reanderCard =()=>{
-   const  a = [1,2,3]
-  
-
-  return  a.map(( value )=>{
-      return <>
-    <div className="flex p-5 border-[1px] border-[#676767]  flex-col  rounded-md  flex justify-center  content-center self-center text-center shadow-2xl  text-stone-200">
-              <h1 className="text-2xl">This Title </h1>
-              <Image src={image} />
-
-              <div className="flex space-x-4 justify-center mt-2">
-                <button className=" border-2 border-[#676767]  min-w-[5rem] p-3 rounded-lg">
-                  Submit{" "}
-                </button>
-                <button className=" border-2 border-[#676767] min-w-[5rem]  p-3 rounded-lg">
-                  View{" "}
-                </button>
-              </div>
+            <div className="flex space-x-4 justify-center mt-2">
+              <button className=" border-2 border-[#676767]  min-w-[5rem] p-3 rounded-lg">
+                Submit{" "}
+              </button>
+              <button className=" border-2 border-[#676767] min-w-[5rem]  p-3 rounded-lg">
+                View{" "}
+              </button>
             </div>
-
-
+          </div>
         </>
-      
+      );
+    });
+  };
 
-   })
-  
-  }
-
-  
   return (
     <>
-    <Layout pageTitle="colelction">
-      <div className="flex flex-col  w-full h-full min-h-[100vh]  justify-center  items-center text-stone-200">
-            <h1 className="text-4xl text-[#fff7] text-center p-4"> Kegagalan terbesar adalah ketika tidak berani mencoba </h1>
-            <p className="text-[#fff7]">- Black Wolf Tech Indonesia -</p>
-      </div>
-    </Layout>
-    <Footer2/>
-
+      <Layout pageTitle="colelction">
+        <div className="bagas">
+          <Image className="bagas2" src={bagas} />
+        </div>
+        <div className="flex flex-col  w-full h-full min-h-[100vh]  justify-center  items-center text-stone-200">
+          <h1 className="text-4xl text-[#fff7] text-center p-4">
+            {" "}
+            Kegagalan terbesar adalah ketika tidak berani mencoba{" "}
+          </h1>
+          <p className="text-[#fff7]">- Black Wolf Tech Indonesia -</p>
+        </div>
+      </Layout>
+      <Footer2 />
     </>
   );
 };
